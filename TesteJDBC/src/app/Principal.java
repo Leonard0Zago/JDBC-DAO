a66b19e8140636b949adfbfb1811b7798bf29ec6
Leonardo <110700602+Leonard0Zago@users.noreply.github.com>
2023-11-25 21:56:38 -0300

    Add files via upload
    Parte 2

--- TesteJDBC/src/app/Principal.java
+++ TesteJDBC/src/app/Principal.java
@@ -4,50 +4,76 @@ import java.math.BigDecimal;
 import java.sql.Date;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Scanner;
 import dao.ProdutoDAO;
+import db.DbException;
 import modelo.Produto;
 
 public class Principal {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		SimpleDateFormat fd = new SimpleDateFormat("dd.MM.yyyy");
 		NumberFormat fm = NumberFormat.getCurrencyInstance();
-		
+
 		ProdutoDAO dao = new ProdutoDAO();
+
 		
-		/*
-		 * Produto prod = new Produto(null, "TV modificada", Date.valueOf("2023-10-31"),
-		 * 51, new BigDecimal(123.45)); prod.setIdProd(5); dao.alterar(prod);dao.adicionar(prod);
-		 */
-		
-		//dao.excluir(5);
-		
-		List<Produto> lista = dao.listarTodos();
-		
-		System.out.println("Lista de Contatos");
-		System.out.println("-----------------");
-		
-		for (Produto produto : lista)
-		{
-			System.out.println("Id...........: " + produto.getIdProd());
-			System.out.println("Nome produto.: " + produto.getNomeProd());
-			System.out.println("Data cadastro: " + produto.getDataCadastro());
-			System.out.println("Quantidade...: " + produto.getQuantidade());
-			System.out.println("Preço........: " + fm.format(produto.getPreco()));
-			System.out.println();
-		}
-		
-		Produto prod = dao.buscarPorId(2);
-		if (prod != null)
-		{
-			System.out.println("Id...........: " + prod.getIdProd());
-			System.out.println("Nome produto.: " + prod.getNomeProd());
-			System.out.println("Data cadastro: " + prod.getDataCadastro());
-			System.out.println("Quantidade...: " + prod.getQuantidade());
-			System.out.println("Preço........: " + fm.format(prod.getPreco()));
-			System.out.println();
-		}
+		 Produto pdt = new Produto(null, 
+				 					"Televisão", 
+				 					Date.valueOf("2022-11-25"),
+				 					30, 
+				 					new BigDecimal(5499.99));
+		 
+		  //pdt.setIdProd(5);
+		 
+		 try {
+			 if (dao.excluir(3)) {
+				 System.out.println("Exclusão efetuada com sucesso!");
+			 } else {
+				 System.out.println("Registro não excluido!");
+			 }
+		 } catch (DbException e) {
+			 System.out.println(e.getMessage());
+		 }
+
+			/*
+			 * Map<String, Integer> retorno = new HashMap<>();
+			 * 
+			 * try { retorno = dao.salvar(pdt);
+			 * 
+			 * System.out.println(retorno.get("afetados").toString() +
+			 * "registro(s) afetado(s)"); if (retorno.get("chave") != null) {
+			 * System.out.println("Novo ID gerado: " + retorno.get("chave")); } } catch
+			 * (DbException e) { System.out.println(e.getMessage()); }
+			 */
+
+		 
+//		System.out.println("Informe o nome do produto a pesquisar: ");
+//		Scanner sc = new Scanner(System.in);
+//		String nome = sc.nextLine();
+//		List<Produto> lista = dao.buscarPorNome(nome);
+//		
+//		if (lista.isEmpty()) {
+//			System.out.println("\nTabela está vazia!");
+//		}else {
+//			System.out.println("Lista de Produtos");
+//			System.out.println("# Total de registros: " + dao.getNumeroRegistros());
+//			String msg = "# registros coincidindo com o filtro de busca: " + lista.size();
+//			System.out.println("=" .repeat(msg.length()));
+//			
+//			for (Produto produto : lista) {
+//				System.out.println("Id...........: " + produto.getIdProd());
+//				System.out.println("Nome produto.: " + produto.getNomeProd());
+//				System.out.println("Data cadastro: " + produto.getDataCadastro());
+//				System.out.println("Quantidade...: " + produto.getQuantidade());
+//				System.out.println("Preço........: " + fm.format(produto.getPreco()));
+//				System.out.println();
+//		}
+//
+//		System.out.println("-" .repeat(msg.length()));
+//		System.out.println(msg);
+//		}
 	}
 }
--- TesteJDBC/src/dao/ProdutoDAO.java
+++ TesteJDBC/src/dao/ProdutoDAO.java
@@ -4,9 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import db.DbException;
 import db.FabricaConexao;
 import modelo.Produto;
 
@@ -14,35 +18,56 @@ public class ProdutoDAO implements IProdutoDAO {
 	private Connection conexao;
 
 	@Override
-	public void adicionar(Produto p) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void alterar(Produto p) {
+	public Map<String, Integer> salvar(Produto p) {
 		conexao = FabricaConexao.getConexao();
 		
-		try
-		{
-			String sql = "UPDATE produto SET nome_prod=?, data_cadastro=?, quantidade=?, preco=? " +
-						"WHERE id_prod=?";
-			
-			PreparedStatement smt = this.conexao.prepareStatement(sql);
+		String sql;
+		Map<String, Integer> retorno = new HashMap<>() {{
+			put("afetados", 0);
+			put("chave", null);
+		}};
+		if (p.getIdProd() == null) {
+			sql = "INSERT INTO produto (nome_prod, data_cadastro, quantidade, preco) "
+					+ "VALUES (?, ?, ?, ?);";
+		} else {
+			sql = "UPDATE produto SET nome_prod=?, data_cadastro=?, quantidade=?, preco=? " +
+					"WHERE id_prod=?";
+		}
+		
+		try (
+			PreparedStatement smt = this.conexao.prepareStatement(sql, 
+													Statement.RETURN_GENERATED_KEYS);
+		){
 			smt.setString(1, p.getNomeProd());
 			smt.setDate(2, p.getDataCadastro());
 			smt.setInt(3, p.getQuantidade());
 			smt.setBigDecimal(4, p.getPreco());
 			
-			smt.setInt(5, p.getIdProd());
+			if (p.getIdProd() != null) {
+				smt.setInt(5, p.getIdProd());
+			}
 			
-			smt.execute();
+			conexao.setAutoCommit(false);
+			int registrosAfetados = smt.executeUpdate();
+			retorno.put("afetados", registrosAfetados);
+			
+			try (ResultSet rs = smt.getGeneratedKeys()) {
+				if (rs.next()) {
+					retorno.put("chave", rs.getInt(1));
+				}
+			}
+			
+			conexao.commit();
+			return retorno;
 		}
-		catch (SQLException e)
-		{
-			throw new RuntimeException(e.getMessage());
-		}
-		finally
+		catch (SQLException e) {
+			try {
+				conexao.rollback();
+				throw new DbException("Transação revertida (rolled back)! " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("ihhh, deu ruim! " + e1.getMessage());
+			}
+		} finally
 		{
 			FabricaConexao.fechaConexao();
 		}
@@ -50,24 +75,38 @@ public class ProdutoDAO implements IProdutoDAO {
 	}
 
 	@Override
-	public void excluir(Integer id) {
+	public boolean excluir(Integer id) {
+		System.out.println("Excluindo o registro " + id);
 		conexao = FabricaConexao.getConexao();
+		String sql = "DELETE FROM produto WHERE id_prod=?";
 		
-		try
-		{
-			String sql = "DELETE FROM produto WHERE id_prod=?";
-			
-			PreparedStatement stmt = this.conexao.prepareStatement(sql);
+		try (
+				PreparedStatement stmt = this.conexao.prepareStatement(sql);
+		){
 			stmt.setInt(1, id);
 			
-			stmt.execute();
-		}
-		catch (SQLException e)
-		{
-			throw new RuntimeException(e.getMessage());
-		} finally
-		{
-			FabricaConexao. fechaConexao();
+			conexao.setAutoCommit(false);
+			int registrosAfetados = stmt.executeUpdate();
+			
+			//if (1 == 1) {
+			//		throw new SQLException();
+			//}
+			
+			conexao.commit();
+			
+			return (registrosAfetados == 1);
+		} catch (SQLException e) {
+			try {
+				conexao.rollback();
+				throw new DbException("Transação revertida (rolled back)!\n"
+									+ "Registro " + id + " não foi excluido!!\n"
+									+ e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("ihhh, deu ruim! " + e.getMessage());
+			}
+		} finally {
+
+		FabricaConexao.fechaConexao();
 		}
 	}
 
@@ -75,20 +114,14 @@ public class ProdutoDAO implements IProdutoDAO {
 	public List<Produto> listarTodos() {
 		conexao = FabricaConexao.getConexao();
 		List<Produto> lista = new ArrayList<>();
-		try
-		{
-			String sql = "SELECT * FROM produto";
-			PreparedStatement stmt = this.conexao.prepareStatement(sql);
+		String sql = "SELECT * FROM produto";
 		
+		try (
+			PreparedStatement stmt = this.conexao.prepareStatement(sql);
 			ResultSet rs = stmt.executeQuery();
+		) {
 			while (rs.next()) {
-				Produto ct = new Produto();
-			
-				ct.setIdProd(rs.getInt("id_prod"));
-				ct.setNomeProd(rs.getString("nome_prod"));
-				ct.setDataCadastro(rs.getDate("data_cadastro"));
-				ct.setQuantidade(rs.getInt("quantidade"));
-				ct.setPreco(rs.getBigDecimal("preco"));
+				Produto ct = setaProduto(rs);
 				
 				lista.add(ct);
 			}
@@ -104,29 +137,31 @@ public class ProdutoDAO implements IProdutoDAO {
 		return lista;
 	}
 
+	private Produto setaProduto(ResultSet rs) throws SQLException {
+		Produto ct = new Produto();
+
+		ct.setIdProd(rs.getInt("id_prod"));
+		ct.setNomeProd(rs.getString("nome_prod"));
+		ct.setDataCadastro(rs.getDate("data_cadastro"));
+		ct.setQuantidade(rs.getInt("quantidade"));
+		ct.setPreco(rs.getBigDecimal("preco"));
+		return ct;
+	}
+
 	@Override
 	public Produto buscarPorId(Integer id) {
 		conexao = FabricaConexao.getConexao();
-		
+		String sql = "SELECT * FROM produto WHERE id_prod=?";
 		Produto p = null;
 		
-		try
-		{
-			String sql = "SELECT * FROM produto WHERE id_prod=?";
-			
+		try (
 			PreparedStatement stmt = this.conexao.prepareStatement(sql);
+		) {
 			stmt.setInt(1,  id);
-			
-			ResultSet rs = stmt.executeQuery();
-			if (rs.next())
-			{
-				p = new Produto();
-				
-				p.setIdProd(rs.getInt("id_prod"));
-				p.setNomeProd(rs.getString("nome_prod"));
-				p.setDataCadastro(rs.getDate("data_cadastro"));
-				p.setQuantidade(rs.getInt("quantidade"));
-				p.setPreco(rs.getBigDecimal("preco"));
+			try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) {
+					p = new Produto();
+				}
 			}
 		}
 		catch (SQLException e)
@@ -139,5 +174,51 @@ public class ProdutoDAO implements IProdutoDAO {
 		
 		return p;
 	}
-
+	
+	@Override
+	public int getNumeroRegistros() {
+		conexao = FabricaConexao.getConexao();
+		String sql = "SELECT count(*) FROM produto";
+		int numRegistros = 0;
+		
+		try (
+			PreparedStatement stmt = this.conexao.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+		){
+			if (rs.next()) {
+				numRegistros = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
+		}finally {
+			FabricaConexao.fechaConexao();
+		}
+		
+		return numRegistros;
+	}
+	
+	@Override
+	public List<Produto> buscarPorNome(String nome) {
+		conexao = FabricaConexao.getConexao();
+		List<Produto> lista = new ArrayList<>();
+		String sql = "SELECT * FROM produto WHERE nome_prod LIKE ?";
+		
+		try (
+			PreparedStatement stmt = this.conexao.prepareStatement(sql);
+		){
+			stmt.setString(1,  "%" + nome + "%");
+			try (ResultSet rs = stmt.executeQuery()) {
+				while (rs.next()) {
+					Produto p = setaProduto(rs);
+					lista.add(p);
+				}
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
+		}finally {
+			FabricaConexao.fechaConexao();
+		}
+		
+		return lista;
+	}
 }
